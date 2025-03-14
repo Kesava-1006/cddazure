0056fc7d2ffa89ac3704f8f44d68bcc2a1a1d773
Kesava-1006 <162858215+Kesava-1006@users.noreply.github.com>
2025-03-14 17:03:35 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,12 @@
+def add_numbers(a, b):
+    return a + b
+
+# Predefined numbers
+num1 = 5
+num2 = 10
+
+# Perform addition
+result = add_numbers(num1, num2)
+
+# Print the result
+print(f"Sum of {num1} and {num2} is: {result}")
